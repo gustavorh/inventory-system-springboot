ed7f35500f57cebdc0879bb94225ffa695b00df6
Gustavo R <gustavocars@gmail.com>
2023-02-20 20:47:29 -0300

    Init commit.

---
+++ src/test/java/me/gustavorh/inventory/InventorySystem/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gustavorh.inventory.InventorySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
